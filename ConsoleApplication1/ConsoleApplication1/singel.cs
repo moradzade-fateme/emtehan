c2f1de2f3383e45ee1c740d694d057fec0102d06
zahra <moradzadezahra456@gmail.com>
2019-12-30 13:51:40 +0330

    emt added

---
+++ ConsoleApplication1/ConsoleApplication1/singel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class singel
+    {
+        private string data;
+        private static singel instance = null;
+        public static singel Instance
+        {
+            get {
+                if (instance == null) {
+                    instance = new singel();
+                }
+                return instance;
+                    }
+        }
+        private singel()
+        {
+            data = "hello world";
+        }
+
+        public string getdata()
+        {
+            return data;
+        }
+    }
+}
